9ad4a934f032ae4c431d68d24d999fb55875ad8f
Gabriel Desidério <gabrieldesd7@gmail.com>
2023-04-22 23:08:40 -0300

    setup font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,15 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { Roboto } from 'next/font/google'
 
-export default MyApp
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+}
